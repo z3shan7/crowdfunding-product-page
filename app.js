f81c75c4c0881fe7e72bcd868eb69c49acb2d0e4
Shoaib Zeshan <szeeshan707@gmail.com>
2022-10-14 15:02:05 +0530

    adding more functionalities

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// Navbar
+
+//////////////// Navbar ///////////////
+
 const openBtn = document.querySelector('.logo-icons .ham-img');
 const closeBtn = document.querySelector('.logo-icons .close-img');
 const anchorLinks = document.querySelector('.anchor-links')
@@ -18,11 +20,9 @@ openBtn.addEventListener('click', () => {
         })
     }
 
-
 })
 
-
-// bookmark button
+//////////////// Bookmark Button ///////////////
 
 const btn2 = document.querySelector('#bookmark-btn')
 const btnText = document.querySelector('.button-text')
@@ -31,45 +31,73 @@ const btnText = document.querySelector('.button-text')
 btn2.addEventListener('click', () => {
     btn2.classList.add('active-btn')
     btnText.textContent = 'Bookmarked'
-
 })
 
 
-//  Modal Section
+///////////////  Modal Section ////////////////
 
 const modalContainer = document.querySelector('.modal-container')
-const openModalBtns = document.querySelectorAll('.modal-open')
+const openModalBtns = document.querySelector('.modal-open')
 const closeImg = document.getElementById('closeImg')
+
+
+///////////////  Adding active class ////////////////
+
 const modalColumn = document.querySelectorAll('.modal__column')
-// console.log(modalContainer)
-// console.log(openModalBtns)
-// console.log(closeImg)
-// console.log(modalColumn)
-
-openModalBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        console.log('first')
-        modalContainer.classList.add('modal-container__show-modal')
-    })
-})
-
-closeImg.addEventListener('click', () => {
-    modalContainer.classList.remove('modal-container__show-modal')
-
-})
-
-
 
 modalColumn.forEach((column) => {
-    column.addEventListener('click', (e) => {
-
-        column.classList.remove('active')
-        const selected = e.currentTarget;
-        selected.classList.add('active')
-
-        console.log(selected)
+    column.addEventListener('click', () => {
+        modalColumn.forEach((element) => element.classList.remove('active'))
+        column.classList.add('active')
 
     })
 })
+
+
+///////////////  Selected Reward Section ////////////////
+
+const bambooBtn = document.getElementById('bambooBtn')
+const blackEditionBtn = document.getElementById('blackEditionBtn')
+const bambooStand = document.querySelector('.modal__bamboo-Stand');
+const blackEdition = document.querySelector('.modal__black-Edition');
+
+
+bambooBtn.addEventListener('click', () => {
+    openModal()
+    bambooStand.classList.add('active')
+
+})
+
+blackEditionBtn.addEventListener('click', () => {
+    openModal()
+    blackEdition.classList.add('active')
+
+})
+
+///////////////  Open close modal functionalities ////////////////
+
+const openModal = () => {
+    modalContainer.classList.add('modal-container__show-modal')
+}
+
+
+const closeModalBtn = () => {
+    modalContainer.classList.remove('modal-container__show-modal')
+    modalColumn.forEach(item => item.classList.remove('active'))
+
+}
+
+
+///////////////  Open close Modal EventListener ////////////////
+
+openModalBtns.addEventListener('click', openModal)
+closeImg.addEventListener('click', closeModalBtn)
+
+
+
+
+
+
+
 
 
