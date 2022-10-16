a60f7e71b3275ecf2496e461b3f8b2d955d55e0c
Shoaib Zeshan <szeeshan707@gmail.com>
2022-10-16 12:24:00 +0530

    adding more functionalities

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
 //////////////// Navbar ///////////////
 
 const openBtn = document.querySelector('.logo-icons .ham-img');
 const closeBtn = document.querySelector('.logo-icons .close-img');
-const anchorLinks = document.querySelector('.anchor-links')
-
+const anchorLinks = document.querySelector('.anchor-links');
 
 openBtn.addEventListener('click', () => {
     anchorLinks.classList.add('show-links')
@@ -27,19 +25,52 @@ openBtn.addEventListener('click', () => {
 const btn2 = document.querySelector('#bookmark-btn')
 const btnText = document.querySelector('.button-text')
 
-
 btn2.addEventListener('click', () => {
     btn2.classList.add('active-btn')
     btnText.textContent = 'Bookmarked'
 })
-
 
 ///////////////  Modal Section ////////////////
 
 const modalContainer = document.querySelector('.modal-container')
 const openModalBtns = document.querySelector('.modal-open')
 const closeImg = document.getElementById('closeImg')
+const thankuModal = document.querySelector('.thanku-modal')
+const modalBtn = document.querySelectorAll('.modal__btn')
+const pledgeBtn = document.getElementById('pledgeBtn')
+const thankuBtn = document.getElementById('thankuBtn')
+console.log(pledgeBtn)
 
+///////////////  Open close modal functionalities ////////////////
+
+const openModal = () => {
+    modalContainer.classList.add('modal-container__show-modal')
+}
+const closeModalBtn = () => {
+    modalContainer.classList.remove('modal-container__show-modal')
+    modalColumn.forEach(item => item.classList.remove('active'))
+}
+const thankuPopup = () => {
+    // const modalBtn = [...document.querySelectorAll('.modal__btn')].disabled = false
+
+    thankuModal.classList.add('thanku-modal__show-check-modal')
+    modalContainer.classList.remove('modal-container__show-modal')
+}
+const closePopup = () => {
+    thankuModal.classList.remove('thanku-modal__show-check-modal')
+}
+
+///////////////  Open close Modal EventListener ////////////////
+
+openModalBtns.addEventListener('click', openModal)
+closeImg.addEventListener('click', closeModalBtn)
+modalBtn.forEach(btn => btn.addEventListener('submit', thankuPopup))
+pledgeBtn.addEventListener('click', () => {
+    thankuPopup()
+    // console.log('backed')
+    addingReward()
+})
+thankuBtn.addEventListener('click', closePopup)
 
 ///////////////  Adding active class ////////////////
 
@@ -53,14 +84,12 @@ modalColumn.forEach((column) => {
     })
 })
 
-
 ///////////////  Selected Reward Section ////////////////
 
 const bambooBtn = document.getElementById('bambooBtn')
 const blackEditionBtn = document.getElementById('blackEditionBtn')
 const bambooStand = document.querySelector('.modal__bamboo-Stand');
 const blackEdition = document.querySelector('.modal__black-Edition');
-
 
 bambooBtn.addEventListener('click', () => {
     openModal()
@@ -74,24 +103,80 @@ blackEditionBtn.addEventListener('click', () => {
 
 })
 
-///////////////  Open close modal functionalities ////////////////
+///////////////  form Input Section ////////////////
 
-const openModal = () => {
-    modalContainer.classList.add('modal-container__show-modal')
+
+const bambooForm = document.getElementById('bamboo-form')
+const blackEditionForm = document.getElementById('black-edition-form')
+const bambooInput = document.getElementById('bamboo-input')
+const blackEditionInput = document.getElementById('blackEdition-input')
+
+console.log(bambooForm)
+console.log(blackEditionInput.value)
+
+// modalBtn.disabled = true
+
+function checkLength(input, min, max) {
+    if (input.value < min || input.value.contains === 'aaaa') {
+        const modalBtn = document.querySelectorAll('.modal__btn').disabled = true
+        console.log('working')
+    }
+    else {
+        (input.value >= max)
+        addingReward()
+        closeModalBtn()
+
+    }
+}
+
+bambooForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkLength(bambooInput, 25, 26)
+
+})
+
+blackEditionForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkLength(blackEditionInput, 75, 76)
+})
+bambooInput.addEventListener('keydown', (e) => {
+    inputInNumb(e)
+})
+blackEditionInput.addEventListener('keydown', (e) => {
+    inputInNumb(e)
+})
+
+const inputInNumb = (e) => {
+    let char = String.fromCharCode(e.which);
+    if (!(/[0-9]/.test(char))) {
+        e.preventDefault()
+    }
+
 }
 
 
-const closeModalBtn = () => {
-    modalContainer.classList.remove('modal-container__show-modal')
-    modalColumn.forEach(item => item.classList.remove('active'))
+///////////////  Adding Reward ////////////////
+const fundedAmount = document.getElementById('fundedAmount')
+const totalBackers = document.getElementById('totalBackers')
+const rewardCount = document.getElementById('reward-count')
+
+// console.log('fundedAmount')
+console.log('rewardCount')
+
+
+let currentValue = 5008
+const addingReward = () => {
+    let innerValue = currentValue++
+    totalBackers.innerHTML = parseInt(innerValue)
+}
+
+const rewardLeft = () => {
 
 }
 
 
-///////////////  Open close Modal EventListener ////////////////
 
-openModalBtns.addEventListener('click', openModal)
-closeImg.addEventListener('click', closeModalBtn)
+
 
 
 
